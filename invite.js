2e9f3ce701ce2f5d96b1c3e3fbfbfd9288cb4e72
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-14 19:47:04 -0400

    Update invite.js

--- invite.js
+++ invite.js
@@ -14,7 +14,7 @@ client.on('message', async (message, member) => {
 
   if (message.content === `${prefix}help`) {
 	let helpembed = new Discord.RichEmbed()
-	.setTitle("☑ Help Bar")
+	.setAuthor("☑ Help Bar")
 	.setDescription("Below are the commands for this bot, enjoy and use them respectfully.")
 	.setColor("#7289da")
 	.setThumbnail()
