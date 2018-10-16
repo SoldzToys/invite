e19081969abb7731722f51a3fa7c6cff373ff2cf
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-16 16:08:27 -0400

    Update invite.js

--- invite.js
+++ invite.js
@@ -53,7 +53,7 @@ client.on('message', async (message, member) => {
     .addField("Guilds", client.guilds.size, true)
     .addField("Users", client.users.size, true)
     .addField("Date Of Creation", client.user.createdAt.toLocaleString(), true)
-    .setFooter("Created By @Dawn.Bots.INC", "https://i.imgur.com/LQsXmSX.png")
+    .setFooter("Created By @Dawn.Bots.INC", "https://i.imgur.com/38YuzFQ.png")
     .setTimestamp(); 
     return message.channel.send(botembed);
   }
